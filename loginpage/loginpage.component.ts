417c9767053aabd2ab0bd59fead7aa1b6a63c1a2
Pavithra <83366596+Pavithra1512@users.noreply.github.com>
2022-12-02 17:05:47 +0530

    Add files via upload

---
+++ loginpage/loginpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DemoService } from '../demo.service';
+
+@Component({
+  selector: 'app-loginpage',
+  templateUrl: './loginpage.component.html',
+  styleUrls: ['./loginpage.component.css']
+})
+export class LoginpageComponent {
+
+}
